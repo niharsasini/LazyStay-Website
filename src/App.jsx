7ca474207e0773772cedcb9778121fcca61ca018
niharsasini <niharsasini@gmail.com>
2025-07-05 16:09:22 +0530

    Fix: Updated import paths to match lowercase 'common' folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Topbar from "./components/common/Topbar.jsx";
-import Navbar from "./components/Common/Navbar";
-import Footer from "./components/Common/Footer";
+import Navbar from "./components/common/Navbar.jsx";
+import Footer from "./components/common/Footer.jsx";
 
 import AppRouter from "./navigation/AppRouter";
 
